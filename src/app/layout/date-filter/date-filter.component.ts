e2c08a98542949487d528e04991d924d776d67f3
Jasmin Cordeiro <jasmin.cordeiro@asaas.com.br>
2025-02-17 21:46:52 -0300

    bug data resolved

--- src/app/layout/date-filter/date-filter.component.ts
+++ src/app/layout/date-filter/date-filter.component.ts
@@ -1,30 +1,41 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { format } from 'date-fns';
+import { Component, EventEmitter, Output } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { format } from "date-fns";
 
 @Component({
-  selector: 'app-date-filter',
+  selector: "app-date-filter",
   imports: [FormsModule],
-  templateUrl: './date-filter.component.html',
-  styleUrl: './date-filter.component.scss'
+  templateUrl: "./date-filter.component.html",
+  styleUrl: "./date-filter.component.scss",
 })
 export class DateFilterComponent {
+  @Output() dateFilterChanged = new EventEmitter<{
+    startDate: string;
+    endDate: string;
+  }>();
 
-  @Output() dateFilterChanged = new EventEmitter<{ startDate: string, endDate: string }>();
-
-  selectedDateRange = { startDate: '', endDate: '' };
+  selectedDateRange = { startDate: "", endDate: "" };
 
   constructor() {
-    this.setDateRange(7); 
+    this.setDateRange(7);
   }
 
   setDateRange(days: number) {
     const today = new Date();
-    const startDate = new Date();
-    startDate.setDate(today.getDate() - days);
+    today.setHours(0, 0, 0, 0);
+    const startDate = new Date(today);
 
-    this.selectedDateRange.startDate = format(startDate, 'yyyy-MM-dd');
-    this.selectedDateRange.endDate = format(today, 'yyyy-MM-dd');
+    if (days === 1) {
+      startDate.setDate(today.getDate() - 1);
+
+      this.selectedDateRange.startDate = format(startDate, "yyyy-MM-dd");
+      this.selectedDateRange.endDate = format(startDate, "yyyy-MM-dd"); 
+    } else {
+      startDate.setDate(today.getDate() - days);
+
+      this.selectedDateRange.startDate = format(startDate, "yyyy-MM-dd");
+      this.selectedDateRange.endDate = format(today, "yyyy-MM-dd");
+    }
 
     this.emitDateChange();
   }
@@ -37,6 +48,4 @@ export class DateFilterComponent {
     console.log("Data selecionada:", this.selectedDateRange);
     this.dateFilterChanged.emit(this.selectedDateRange);
   }
-  
-
 }
--- src/pages/expense-form/expense-form.component.ts
+++ src/pages/expense-form/expense-form.component.ts
@@ -1,37 +1,35 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
 import {
   FormBuilder,
   FormControl,
   FormGroup,
   ReactiveFormsModule,
   Validators,
-} from '@angular/forms';
-import { ExpenseService } from '../../core/services/expense.service';
-import { IExpense } from '../../app/core/models/common.model';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Category } from '../../app/core/models/category.model';
+} from "@angular/forms";
+import { ExpenseService } from "../../core/services/expense.service";
+import { IExpense } from "../../app/core/models/common.model";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Category } from "../../app/core/models/category.model";
 
 @Component({
-  selector: 'app-expense-form',
+  selector: "app-expense-form",
   imports: [CommonModule, ReactiveFormsModule],
-  templateUrl: './expense-form.component.html',
-  styleUrl: './expense-form.component.scss',
+  templateUrl: "./expense-form.component.html",
+  styleUrl: "./expense-form.component.scss",
 })
 export class ExpenseFormComponent {
   expenseForm!: FormGroup;
-  expenseId = '';
+  expenseId = "";
   selectedCategory: any = null;
 
-
- categories: Category[] = [
-    { id: 1, name: 'Saúde', color: '#71D06D', limit: '500.00' },
-    { id: 2, name: 'Alimentação', color: '#FFC48C', limit: '1000.00' },
-    { id: 3, name: 'Educação', color: '#D6A8FF', limit: '800.00' },
-    { id: 4, name: 'Lazer', color: '#6D6727' , limit: '600.00' },
-    { id: 5, name: 'Pet', color: '#D2B48C' , limit: '300.00' }
+  categories: Category[] = [
+    { id: 1, name: "Saúde", color: "#71D06D", limit: "500.00" },
+    { id: 2, name: "Alimentação", color: "#FFC48C", limit: "1000.00" },
+    { id: 3, name: "Educação", color: "#D6A8FF", limit: "800.00" },
+    { id: 4, name: "Lazer", color: "#6D6727", limit: "600.00" },
+    { id: 5, name: "Pet", color: "#D2B48C", limit: "300.00" },
   ];
-
 
   constructor(
     private fb: FormBuilder,
@@ -39,59 +37,70 @@ export class ExpenseFormComponent {
     private router: Router,
     private activatedRoute: ActivatedRoute
   ) {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
     this.expenseForm = this.fb.group({
-      price: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+(\\.[0-9]{1,2})?$')]),
-      title: new FormControl('', Validators.required),
+      price: new FormControl("", [
+        Validators.required,
+        Validators.pattern("^[0-9]+(\\.[0-9]{1,2})?$"),
+      ]),
+      title: new FormControl("", Validators.required),
       category: new FormControl(null, Validators.required),
-      date: new FormControl(new Date().toISOString().split('T')[0], Validators.required) // Define a data atual como padrão
+      date: new FormControl(
+        new Date(today).toISOString().split("T")[0],
+        Validators.required
+      ), 
     });
   }
 
- selectCategory(category: any) {
-    this.selectedCategory = category; 
-    this.expenseForm.patchValue({ category: category.id }); 
+  selectCategory(category: any) {
+    this.selectedCategory = category;
+    this.expenseForm.patchValue({ category: category.id });
   }
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.activatedRoute.params.subscribe({
-      next:(params)=>{
-        this.expenseId = params['id'];
+      next: (params) => {
+        this.expenseId = params["id"];
         this.getExpensive(this.expenseId);
-      }
-    })}
-
-    onSubmit() {
-      if (this.expenseForm.valid) {
-        let formData = { ...this.expenseForm.value };
-    
-        if (formData.date) {
-          formData.date = new Date(formData.date).toISOString();
-        }
-    
-        if (this.expenseId) {
-          this.expenseService.updateExpense(this.expenseId, formData);
-        } else {
-          this.expenseService.addExpense(formData);
-        }
-        this.router.navigate(['/dashboard']);
-      } else {
-        this.expenseForm.markAllAsTouched();
-      }
-    }
-    
-  
-  getExpensive(key: string) {
-    this.expenseService.getExpense(key).snapshotChanges().subscribe({
-      next: (data) => {
-        let expense = data.payload.toJSON() as IExpense;
-        
-        if (expense.date) {
-          expense.date = new Date(expense.date).toISOString().split('T')[0];
-        }
-  
-        this.expenseForm.patchValue(expense);
-      }
+      },
     });
   }
-  
+
+  onSubmit() {
+    if (this.expenseForm.valid) {
+      let formData = { ...this.expenseForm.value };
+
+      if (formData.date) {
+        formData.date = new Date(formData.date).toISOString();
+      }
+
+      if (this.expenseId) {
+        this.expenseService.updateExpense(this.expenseId, formData);
+      } else {
+        this.expenseService.addExpense(formData);
+      }
+      this.router.navigate(["/dashboard"]);
+    } else {
+      this.expenseForm.markAllAsTouched();
+    }
+  }
+
+  getExpensive(key: string) {
+    this.expenseService
+      .getExpense(key)
+      .snapshotChanges()
+      .subscribe({
+        next: (data) => {
+          let expense = data.payload.toJSON() as IExpense;
+
+          if (expense.date) {
+            expense.date = new Date(expense.date).toISOString().split("T")[0];
+          }
+
+          this.expenseForm.patchValue(expense);
+        },
+      });
+  }
 }
